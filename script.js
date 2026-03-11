ec2b93d972fbd7fb812081b827031c5c5d88ee2b
abhinav-mix <bhainew124@gmail.com>
2026-03-11 16:31:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const movies = [
         description: "<strong>Director:</strong> Prashant<br><strong>Hero:</strong> Prashant | <strong>Heroine:</strong> Himani<br><strong>Cast:</strong> Sourya, Manish Solanki, Sobha, Gabbar, Khushi, Bhumika & others.<br><strong>Address:</strong> Khurja Cinema, Moon City",
         whatsappNumber: "917617605850",
         silverPrice: 120,
-        goldPrice: 190,
+        goldPrice: 249,
         diamondPrice: 290,
         status: "now_showing", // Options: "now_showing" 
         likes: 245
@@ -313,5 +313,6 @@ if (heroSection) {
         heroSection.style.background = `url('${heroImages[currentHeroImageIndex]}') center/contain no-repeat #000`;
     }, 2500);
 }
+
 
 
